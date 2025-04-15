358f9698b756a0c2306be5d7cfd50ecc55f96405
jezryl <r_jezryl15@gmail.com>
2025-04-15 11:17:12 +0800

    Added room filter field and dropdown room filter

--- src/app/portal/room/page.tsx
+++ src/app/portal/room/page.tsx
@@ -14,9 +14,17 @@ import {
 import { Pagination } from "@/components/pagination";
 import { Badge } from "@/components/ui/badge";
 import { RoomStatus } from "@/types/room.types";
+import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
 
 export default function Room() {
-  const { errors, roomList } = useRoom(); 
+  const { errors, roomList, filterRooms } = useRoom();
 
   const rowsPerPage = 15;
   const [currentPage, setCurrentPage] = useState(1);
@@ -30,6 +38,9 @@ export default function Room() {
       <div className="flex space-x-2">
         <div className="relative w-full">Room Management</div>
       </div>
+      <div className="text-end">
+        <Input onChange={(e) => filterRooms(e.target.value)}></Input>
+      </div>
       {errors.room && (
         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
           {errors.room}
@@ -41,9 +52,57 @@ export default function Room() {
           <TableHeader>
             <TableRow>
               <TableHead className="font-black">Room Number</TableHead>
-              <TableHead className="font-black">Type</TableHead>
+              <TableHead className="font-black">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="flex gap-1 cursor-pointer hover:underline">
+                    <span>Type</span> <ChevronDown></ChevronDown>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem onSelect={() => filterRooms("")}>
+                      All
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onSelect={() => filterRooms("Standard")}>
+                      Standard
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onSelect={() => filterRooms("Barkada")}>
+                      Barkada
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onSelect={() => filterRooms("Family")}>
+                      Family
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onSelect={() => filterRooms("Executive")}>
+                      Executive
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableHead>
               <TableHead className="font-black">Rate</TableHead>
-              <TableHead className="font-black">Status</TableHead>
+              <TableHead className="font-black">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="flex gap-1 cursor-pointer hover:underline">
+                    <span>Status</span> <ChevronDown></ChevronDown>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem onSelect={() => filterRooms("")}>
+                      All
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onSelect={() => filterRooms("Available")}>
+                      Available
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onSelect={() => filterRooms("Occupied")}>
+                      Occupied
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onSelect={() => filterRooms("Reserved")}>
+                      Reserved
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      onSelect={() => filterRooms("Maintenance")}
+                    >
+                      Maintenance
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableHead>
               <TableHead className="font-black">Pax</TableHead>
             </TableRow>
           </TableHeader>
@@ -52,13 +111,17 @@ export default function Room() {
               let statusColor = "text-gray-500";
 
               if (room.status === RoomStatus.AVAILABLE) {
-                statusColor = "bg-green-500 hover:bg-green-700 text-white font-bold";
+                statusColor =
+                  "bg-green-500 hover:bg-green-700 text-white font-bold";
               } else if (room.status === RoomStatus.OCCUPIED) {
-                statusColor = "bg-red-500 hover:bg-red-700 text-white font-bold";
+                statusColor =
+                  "bg-red-500 hover:bg-red-700 text-white font-bold";
               } else if (room.status === RoomStatus.RESERVED) {
-                statusColor = "bg-yellow-500 hover:bg-yellow-700 text-white font-bold";
+                statusColor =
+                  "bg-yellow-500 hover:bg-yellow-700 text-white font-bold";
               } else if (room.status === RoomStatus.MAINTENANCE) {
-                statusColor = "bg-gray-500 hover:bg-gray-700 text-white font-bold";
+                statusColor =
+                  "bg-gray-500 hover:bg-gray-700 text-white font-bold";
               }
 
               return (
--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -2,88 +2,111 @@ import { useEffect, useState } from "react";
 import supabaseBrowser from "@/lib/supabase/client";
 import { RoomsProps } from "@/types/room.types";
 
-
-
 export function useRoom() {
-    //TO-DO
-    // const [roomData, setRoomData] = useState({
-    //     room: "",
-    //     type: "",
-    //     price: "",
-    //     status: "",
-    //     pax: "",
-    // });
-    const [availableRooms, setAvailableRooms] = useState<RoomsProps[]>([]);
-    const [roomList, setRoomList] = useState<RoomsProps[]>([]);
-    const [errors, setErrors] = useState({
-        room: ""
-    });
-    
-    useEffect(() => {
-        const fetchRooms = async () => {
-        const supabase = supabaseBrowser();
+  //TO-DO
+  // const [roomData, setRoomData] = useState({
+  //     room: "",
+  //     type: "",
+  //     price: "",
+  //     status: "",
+  //     pax: "",
+  // });
+  const [availableRooms, setAvailableRooms] = useState<RoomsProps[]>([]);
+  const [roomList, setRoomList] = useState<RoomsProps[]>([]);
+  const [errors, setErrors] = useState({
+    room: "",
+  });
 
-            const { data, error } = await supabase
-                .from("room_details")
-                .select("room, type, price, status, pax")
-                .order("room", { ascending: true });
+  useEffect(() => {
+    const fetchRooms = async () => {
+      const supabase = supabaseBrowser();
 
-            if (error) {
-                setErrors((prev) => ({
-                  ...prev,
-                  room: "Error fetching rooms. Please try again later.",
-                }));
-                return;
-            }
-            const transformedData: RoomsProps[] = data.map((room) => {
-                return {
-                  room: room.room,
-                  type: room.type,
-                  price: room.price,
-                  status: room.status.toUpperCase(),
-                  pax: room.pax,
-                };
-            });
-                
-            setRoomList(transformedData);
+      const { data, error } = await supabase
+        .from("room_details")
+        .select("room, type, price, status, pax")
+        .order("room", { ascending: true });
+
+      if (error) {
+        setErrors((prev) => ({
+          ...prev,
+          room: "Error fetching rooms. Please try again later.",
+        }));
+        return;
+      }
+      const transformedData: RoomsProps[] = data.map((room) => {
+        return {
+          room: room.room,
+          type: room.type,
+          price: room.price,
+          status: room.status.toUpperCase(),
+          pax: room.pax,
         };
-        fetchRooms();
-    }, []);
+      });
 
-     const fetchAvailableRooms = async () => {
-       const supabase = supabaseBrowser();
-
-       const { data, error } = await supabase
-         .from("room_details")
-         .select("room, type, price, status, pax")
-         .eq("status", "available")
-         .order("room", { ascending: true });
-
-       if (error) {
-         setErrors((prev) => ({
-           ...prev,
-           room: "Error fetching available rooms. Please try again later.",
-         }));
-         return;
-       }
-
-       const transformedAvailableRooms: RoomsProps[] = data.map((room) => ({
-         room: room.room,
-         type: room.type,
-         price: room.price,
-         status: room.status.toUpperCase(),
-         pax: room.pax,
-       }));
-
-       setAvailableRooms(transformedAvailableRooms);
-     };
-
-    return {
-        setRoomList,
-        availableRooms,
-        roomList,
-        fetchAvailableRooms,
-        errors
+      setRoomList(transformedData);
     };
+    fetchRooms();
+  }, []);
+
+  const fetchAvailableRooms = async () => {
+    const supabase = supabaseBrowser();
+
+    const { data, error } = await supabase
+      .from("room_details")
+      .select("room, type, price, status, pax")
+      .eq("status", "available")
+      .order("room", { ascending: true });
+
+    if (error) {
+      setErrors((prev) => ({
+        ...prev,
+        room: "Error fetching available rooms. Please try again later.",
+      }));
+      return;
+    }
+
+    const transformedAvailableRooms: RoomsProps[] = data.map((room) => ({
+      room: room.room,
+      type: room.type,
+      price: room.price,
+      status: room.status.toUpperCase(),
+      pax: room.pax,
+    }));
+
+    setAvailableRooms(transformedAvailableRooms);
+  };
+
+  const filterRooms = async (searchFilter: string) => {
+    const supabase = supabaseBrowser();
+
+    const { data: filteredRoomsData, error } = await supabase
+      .from("room_details")
+      .select("*")
+      .or(`status.ilike.%${searchFilter}%, type.ilike.%${searchFilter}%`)
+      .order("room", { ascending: true });
+
+    if (filteredRoomsData) {
+      const transformedData: RoomsProps[] = filteredRoomsData.map((room) => {
+        return {
+          room: room.room,
+          type: room.type,
+          price: room.price,
+          status: room.status.toUpperCase(),
+          pax: room.pax,
+        };
+      });
+      setRoomList(transformedData);
+    } else {
+      console.log("Filter Error: ", error);
+    }
+  };
+
+  return {
+    setRoomList,
+    availableRooms,
+    roomList,
+    fetchAvailableRooms,
+    errors,
+    filterRooms,
+  };
 }
-  
